c3c59f47c44375affe96d76e70138b6c63040c5d
men_l <men_long@yahoo.com>
2025-07-05 11:22:43 -0400

    update

--- Day06-AZ-Terraform/storage_acc.tf
+++ Day06-AZ-Terraform/storage_acc.tf
@@ -4,6 +4,3 @@ data "azurerm_storage_account" "existing" {
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
-output "resource_group_name" {
-  value = data.azurerm_resource_group.example.name
-}
